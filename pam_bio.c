7f695270a5a99f5232a95eb601d5e57fb634ff89
ccat3z <fzhang.chn@foxmail.com>
2021-01-16 14:07:57 +0800

    implement pam_sm_setcred

--- pam_bio.c
+++ pam_bio.c
@@ -4,6 +4,9 @@
 extern int do_authenticate (pam_handle_t* pamh, int flags, gint argc, gchar** argv);
 
 PAM_EXTERN int pam_sm_authenticate (pam_handle_t* pamh, int flags, int argc, const char** argv) {
-
     return do_authenticate(pamh, flags, argc, argv);
+}
+
+PAM_EXTERN int pam_sm_setcred(pam_handle_t *pamh, int flags, int argc, const char **argv) {
+    return PAM_SUCCESS;
 }
